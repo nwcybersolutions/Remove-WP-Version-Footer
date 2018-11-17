a70be1b37cce6371269c496a0f3ac11aba5c7b4e
Northwest Cyber Solutions <31363854+nwcybersolutions@users.noreply.github.com>
2018-11-16 23:17:33 -0800

    Create functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ Plugin URI: https://github.com/nwcybersolutions/Remove-WP-Version-Footer
 Description: Removes Version from the Footer in the Admin Dashboard
 Author: Northwest Cyber Solutions
 Author URI: https://nwcybersolutions.com
-Version: 1.0.1
+Version: 1.0
 License: MIT
 License URI: https://opensource.org/licenses/MIT
 Text Domain: Remove WP Version Footer
@@ -19,3 +19,4 @@ function remove_versionfooter() {
         remove_filter( 'update_footer', 'core_update_footer' ); 
     }
 add_action( 'admin_menu', 'remove_versionfooter' );
+
